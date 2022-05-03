ab4e6629a56776a879f4208ff9a40dd782e73145
Isabella Poloni <isabellapolonif@gmail.com>
2022-05-03 10:37:22 -0300

    tipos primitivos

---
+++ mundo1/exe003.py
@@ -0,0 +1,16 @@
+
+n1 = int(input ('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+# print('A soma entre', n1, 'e', n2, 'vale', s) - o formato abaixo é uma forma melhor de utilizar
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+
+
+#abaixo é alguns testes feitos e anotções em aula 
+#type = float(real/parte flutuante), int(inteiro), bool(True and False *sempre a primeira letra em maiúscula*/ *quando o input for vazio o valor automaticamente é false*), str(string)
+
+n = float(input('Digite um valor: ')) 
+print(type(n))
+
+
+
