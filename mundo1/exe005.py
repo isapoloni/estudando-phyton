1bd534287a2c28a389cb4668c3958c52237a869a
Isabella Poloni <isabellapolonif@gmail.com>
2022-05-04 09:41:38 -0300

    space

--- mundo1/exe005.py
+++ mundo1/exe005.py
@@ -13,4 +13,4 @@ print('Divisão inteira {} e potencia {}'.format(n1//n2, n1**n2))
 #desafio-tabuada
 
 n = int(input('Qual tabuada vc quer aprender? '))
-print('1 x {} = {} \n2 x {} = {} \n3 x {} = {} \n4 x {} = {} \n5 x {} = {} \n6 x {} = {} \n7 x {} = {} \n8 x {} = {} \n9 x {} = {} \n10 x {} = {}'.format(n, 1 * n, n, 2 * n, n, 3*n, n, 4*n, n, 5*n, n, 6*n, n, 7*n, n, 8*n, n, 9*n, n, 10*n))
+print(' 1 x {} = {} \n 2 x {} = {} \n 3 x {} = {} \n 4 x {} = {} \n 5 x {} = {} \n 6 x {} = {} \n 7 x {} = {} \n 8 x {} = {} \n 9 x {} = {} \n 10 x {} = {}'.format(n, 1 * n, n, 2 * n, n, 3*n, n, 4*n, n, 5*n, n, 6*n, n, 7*n, n, 8*n, n, 9*n, n, 10*n))
