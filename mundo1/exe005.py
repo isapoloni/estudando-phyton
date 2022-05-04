391a85a47ef6e70f0684022257318cf09fd08c16
Isabella Poloni <isabellapolonif@gmail.com>
2022-05-04 09:42:45 -0300

    aprendendo mais sobre operadores aritméticos e formatações

--- mundo1/exe005.py
+++ mundo1/exe005.py
@@ -11,6 +11,5 @@ print('Divisão inteira {} e potencia {}'.format(n1//n2, n1**n2))
     # print // {:20} ->  quantidade em caracteres(espaços) | {:.3f} o f é de "flutuante" para determinar casas decimais // *alinhamentos* (> a direita) (< a esquerda) (^ ao centro) // *quebras de linha* (\n quebrar a linha) (end='' não quebrar a linha)
 
 #desafio-tabuada
-
 n = int(input('Qual tabuada vc quer aprender? '))
 print(' 1 x {} = {} \n 2 x {} = {} \n 3 x {} = {} \n 4 x {} = {} \n 5 x {} = {} \n 6 x {} = {} \n 7 x {} = {} \n 8 x {} = {} \n 9 x {} = {} \n 10 x {} = {}'.format(n, 1 * n, n, 2 * n, n, 3*n, n, 4*n, n, 5*n, n, 6*n, n, 7*n, n, 8*n, n, 9*n, n, 10*n))
